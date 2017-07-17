f5242e42ef4d85f184ced5276fabccb9a447d8a8
Jay Weeks <jay.patrick.weeks@gmail.com>
2017-07-17 16:15:56 -0400

    Update word vec drawing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { PI, sin, cos } = Math
+const { PI, sin, cos, floor } = Math
 
 const canvas = document.createElement('canvas')
 const ctx = canvas.getContext('2d')
@@ -9,7 +9,9 @@ function animate () {
   window.requestAnimationFrame(animate)
 }
 
+// TODO: Fix end loop ... maybe something with wordVecLength?
 function draw () {
+  const wordVecLength = 300
   const { width, height, data, vecOffset } = state
   const { sentLengths, wordVectors } = data
   const sentLength = sentLengths[state.sentIndex++]
@@ -17,38 +19,41 @@ function draw () {
   if (sentLength == null) {
     state.sentIndex = 0
     state.vecOffset = 0
+    console.log('loop')
     return
   }
 
   ctx.setTransform(1, 0, 0, 1, 0, 0)
-  ctx.globalAlpha = 0.1
+  ctx.globalAlpha = 0.01
   ctx.fillStyle = '#1b1a22'
   ctx.fillRect(0, 0, width, height)
   // ctx.clearRect(0, 0, width, height)
 
   ctx.translate(width / 2, height / 2)
-  ctx.globalAlpha = 0.6
+  ctx.globalAlpha = 0.4
   ctx.strokeStyle = '#ffffff'
   ctx.lineWidth = 1
 
-  state.vecOffset += sentLength
+  const fullSentLength = sentLength * wordVecLength
+  state.vecOffset += fullSentLength
   drawVector(wordVectors,
-    [vecOffset, vecOffset + sentLength],
-    [100, 300])
+    [vecOffset, vecOffset + fullSentLength],
+    [100, 300], 60)
 }
 
-function drawVector (vec, valRange, radRange) {
+function drawVector (vec, valRange, radRange, interval=1) {
   const start = valRange[0]
-  const count = valRange[1] - start
+  const count = floor((valRange[1] - start) / interval)
   const angleEnd = PI * 2
   const radStart = radRange[0]
   const radLen = radRange[1] - radStart
 
   ctx.beginPath()
   for (let i = -1; i < count; i++) {
-    const index = start + (i === -1 ? count - 1 : i)
-    const radius = vec[index] * radLen + radStart
-    const angle = i / count * angleEnd
+    const index = start + (i === -1 ? count - 1 : i) * interval
+    const intensity = mapLinear(-2, 1, 0, 1, vec[index])
+    const radius = intensity * radLen + radStart
+    const angle = index / (count * interval) * angleEnd
     const x = sin(angle) * radius
     const y = cos(angle) * radius
 
@@ -56,6 +61,11 @@ function drawVector (vec, valRange, radRange) {
     else ctx.lineTo(x, y)
   }
   ctx.stroke()
+}
+
+// Linear mapping from range <a1, a2> to range <b1, b2>
+function mapLinear (a1, a2, b1, b2, x) {
+  return b1 + (x - a1) * (b2 - b1) / (a2 - a1)
 }
 
 function resize () {
