54e6c241aed299f72284185cbb88e708d15831af
Jay Weeks <jay.patrick.weeks@gmail.com>
2017-07-18 13:45:07 -0400

    Add chroma, draw concentric rings

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
+const chroma = require('chroma-js')
+
 const { PI, sin, cos, floor } = Math
 const WORD_VEC_LENGTH = 300
 
 const canvas = document.createElement('canvas')
 const ctx = canvas.getContext('2d')
 const state = {
+  tick: 0,
+  innerRadius: 0,
+  clearColor: chroma('#1b1a22').brighten(0.1),
+  strokeColor: chroma.random().set('hsl.l', 0.8),
   accumWordWeight: new Float32Array(WORD_VEC_LENGTH)
 }
 
 function animate () {
+  state.tick++
   draw()
   window.requestAnimationFrame(animate)
 }
 
 // TODO: Fix end loop ... maybe something with WORD_VEC_LENGTH?
 function draw () {
-  const { width, height, data, accumWordWeight, vecOffset } = state
+  const { width, height, tick, data, accumWordWeight, vecOffset } = state
   const { sentLengths, wordVectors } = data
   const sentLength = 1//sentLengths[state.sentIndex++]
 
@@ -27,28 +34,31 @@ function draw () {
   }
 
   ctx.setTransform(1, 0, 0, 1, 0, 0)
-  ctx.globalAlpha = 0.05
-  ctx.fillStyle = '#1b1a22'
-  ctx.fillRect(0, 0, width, height)
+  // ctx.globalAlpha = 0.02
+  // ctx.fillStyle = state.clearColor.hex()
+  // ctx.fillRect(0, 0, width, height)
   // ctx.clearRect(0, 0, width, height)
 
   ctx.translate(width / 2, height / 2)
-  ctx.globalAlpha = 0.8
-  ctx.strokeStyle = '#ffffff'
+  ctx.globalCompositeOperation = 'overlay'
+  ctx.globalAlpha = 0.1
+  ctx.strokeStyle = state.strokeColor.set('hsl.h', tick * 0.1 % 360).hex()
   ctx.lineWidth = 1
 
+  const innerRadius = state.innerRadius
   const fullSentLength = sentLength * WORD_VEC_LENGTH
   state.vecOffset += fullSentLength
+  state.innerRadius += 0.5
 
   accumWordWeights(accumWordWeight, wordVectors,
     [vecOffset, vecOffset + fullSentLength],
-    0.1, 0.96)
+    0.05, 0.99)
   drawVector(accumWordWeight,
     [0, WORD_VEC_LENGTH],
-    [100, 300], 1)
+    [innerRadius, innerRadius + 240], 1)
 }
 
-function accumWordWeights(out, vec, valRange, weight=1, decay=1) {
+function accumWordWeights (out, vec, valRange, weight=1, decay=1) {
   const start = valRange[0]
   const count = valRange[1] - start
   for (let i = 0; i < count; i++) {
@@ -68,7 +78,7 @@ function drawVector (vec, valRange, radRange, interval=1) {
   ctx.beginPath()
   for (let i = -1; i < count; i++) {
     const index = start + (i === -1 ? count - 1 : i) * interval
-    const intensity = mapLinear(-2, 1, 0, 1, vec[index])
+    const intensity = mapLinear(-2, 2, 0, 1, vec[index])
     const radius = intensity * radLen + radStart
     const angle = index / (count * interval) * angleEnd
     const x = sin(angle) * radius
@@ -92,6 +102,11 @@ function resize () {
   state.height = height
   canvas.width = width
   canvas.height = height
+  Object.assign(canvas.style, {
+    position: 'absolute',
+    top: 0,
+    left: 0
+  })
 }
 
 function fetchData () {
