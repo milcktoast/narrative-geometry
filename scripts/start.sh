088c15dbfc0f1776672064844f2564db19c1c613
Jay Weeks <jay.patrick.weeks@gmail.com>
2017-07-18 13:44:32 -0400

    Add babel

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 PATH=$(npm bin):$PATH
 rm build/bundle-app.js
-budo src/index.js:build/bundle-app.js --live -- -t installify
+budo src/index.js:build/bundle-app.js --live -- -t babelify -t installify
